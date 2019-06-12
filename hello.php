5e549dcf5a5e3ec208af714dff92dc577d68491b
take8 <37664118+take8@users.noreply.github.com>
2019-06-13 00:33:38 +0900

    Hello World for Web

--- hello.php
+++ hello.php
@@ -1,5 +1,19 @@
-<?php
-  echo "Hello World!\n";
-  echo "\n";
-  echo "It runs on Web server!!\n"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+  <title>My First Web App</title>
+</head>
+<body>
+  <h1>My First Web App</h1>
+  <?php
+    echo "Hello World!\n";
+  ?>
+  <br>
+  <?php
+    echo "It runs on Web server!!\n";
+  ?>
+</body>
+</html>
