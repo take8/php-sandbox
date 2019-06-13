947270e767e8b6bcaf66928e02d60ba7f23aed0f
take8 <37664118+take8@users.noreply.github.com>
2019-06-13 12:22:26 +0900

    Form sample

---
+++ action.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+  <title>My Second Web App</title>
+</head>
+<body>
+  <h1>あなたの名前と生まれた月</h1>
+
+  こんにちは、<?php echo $_POST['name'] ?>さん
+  <br>
+  あなたの生まれた月は、<?php echo $_POST['month'] ?>月です。
+
+</body>
+</html>
---
+++ name_month.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+  <title>My Second Web App</title>
+</head>
+<body>
+  <h1>My Second Web App</h1>
+  <form action="action.php" method="post">
+    名前: <input type="text" name="name" />
+    生まれた月: <input type="text" name="month" />
+    <input type="submit" value="送信" />
+  </form>
+</body>
+</html>
